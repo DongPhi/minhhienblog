aee5ba1e311538d10babb07532b716b91858583c
DongPhi <43595603+DongPhi@users.noreply.github.com>
2021-01-26 18:01:49 +0700

    start

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+app.get('/admin', function(req, res) {
+    res.render('pages/admin');
+});
+
+app.listen(7777)
